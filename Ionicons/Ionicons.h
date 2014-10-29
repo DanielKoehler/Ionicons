5f057a37f2122388bb5a3073155127a0c37b6007
Daniel Koehler <dandanarno@gmail.com>
2014-10-29 16:16:04 +0000

    Initial Commit

---
+++ Ionicons/Ionicons.h
@@ -0,0 +1,19 @@
+//
+//  Ionicons.h
+//  Ionicons
+//
+//  Created by Daniel Koehler on 29/10/2014.
+//  Copyright (c) 2014 DanielKoehler. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Ionicons.
+FOUNDATION_EXPORT double IoniconsVersionNumber;
+
+//! Project version string for Ionicons.
+FOUNDATION_EXPORT const unsigned char IoniconsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Ionicons/PublicHeader.h>
+
+
---
+++ IoniconsTests/IoniconsTests.swift
@@ -0,0 +1,36 @@
+//
+//  IoniconsTests.swift
+//  IoniconsTests
+//
+//  Created by Daniel Koehler on 29/10/2014.
+//  Copyright (c) 2014 DanielKoehler. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class IoniconsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
